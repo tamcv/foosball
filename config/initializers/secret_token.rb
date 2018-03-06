a118edf9fd0874e0b9e873e1e9b9db0c7b516aa2
Tam Chau <tam.chau@quoine.com>
2018-03-06 22:18:03 +0700

    create foosball project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Foosball::Application.config.secret_key_base = 'fa17f96ef75cd0d4be6e1d9cff42cc51617ec02a07446a48d1c5494796846c2a30d9dd8832bf43e50e52a659213ee6297e22d3eefeb2e84e62ffd9e32aad19cd'
